3ad9f64b662405f134d2515b79af5c19d7bf39ea
Fawn <sarahmargaret@ymail.com>
2023-04-08 02:43:05 -0400

    added beginning of js

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+const question = document.querySelector('#question');
+const choice = Array.from(document.querySelectorAll('#.choice-text'));
+const progressText = document.querySelector('#progressText');
+const score = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
